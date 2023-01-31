d257c3865b47b1251144e3a8a6a0b634dee54475
Joel Tarzán <joelth368@gmail.com>
2023-01-31 19:50:54 +0100

    Barra de busqueda de ciudades completada

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '7284915465msh8e2f0fa4225cef6p183a41jsn3665066be348',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,44 @@
-const Search = () => {
-    return ("Hello");
+import {AsyncPaginate} from "react-select-async-paginate";
+import {useState} from "react";
+import {GEO_API_URL, geoApiOptions} from "../../api";
+
+const Search = ({onSearchChange}) => {
+
+    const [search, setSearch] = useState(null);
+
+    // Función para ir cargando las opciones conforme se vaya escribiendo mediante peticiones a la api
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?namePrefix=${inputValue}`, geoApiOptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+    }
+
+    // Función para manejar los cambios de búsqueda
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        // Componente importado
+        <AsyncPaginate
+            placeholder = "Search your city..."
+            debounceTimeout = {600}
+            value = {search}
+            onChange = {handleOnChange}
+            loadOptions = {loadOptions}
+        />
+    );
 }
 
 export default Search;
