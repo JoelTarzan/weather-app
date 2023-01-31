8729bc040f7f02055460a89a50c67390aec09cb5
Joel Tarzán <joelth368@gmail.com>
2023-02-01 00:18:27 +0100

    Estilo del componente CurrentWeather creado

---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,41 @@
+import "./current-weather.css"
+
+const CurrentWeather = () => {
+    return (
+        <div className="weather">
+            <div className="top">
+                <div>
+                    <p className="city">Reus</p>
+                    <p className="weather-description">Sunny</p>
+                </div>
+                <img src="icons/01d.png" alt="weather" className="weather-icon"/>
+            </div>
+            <div className="bottom">
+                <p className="temperature">4°C</p>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label top">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like</span>
+                        <span className="parameter-value">7°C</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">12Km/h</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">15%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">32 hPa</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CurrentWeather;
